1b835077976c6cd09be267c1534eab92b482e207
Afanasiev Maksym <80445276+maxafanasiev@users.noreply.github.com>
2025-02-24 15:28:09 +0200

    Update lint.sh

--- lint.sh
+++ lint.sh
@@ -1,4 +1,4 @@
 set -euxo pipefail
 
-poetry run pre-commit install
-poetry run pre-commit run --all-files --show-diff-on-failure
+pre-commit install
+pre-commit run --all-files --show-diff-on-failure
