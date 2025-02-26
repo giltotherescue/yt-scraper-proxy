c1005621712615a8f023a9428bb043aa2fd7968d
maxafanasiev <80445276+maxafanasiev@users.noreply.github.com>
2025-02-26 11:35:09 +0200

    update scripts

--- deploy_scripts/deploy-to-cr.sh
+++ deploy_scripts/deploy-to-cr.sh
@@ -8,9 +8,8 @@ set +o allexport
 # Generate UNIQUE_PREFIX on the fly
 UNIQUE_PREFIX=$(date "+%Y-%m-%d-%H%M%S")
 
-#cd ..
 # Run the gcloud builds submit command with substitutions
 gcloud builds submit --config deploy_scripts/deploy_cloudbuild_cr.yaml \
   --project "$PROJECT_ID" \
   --substitutions \
-    _PROJECT_ID="$PROJECT_ID",_PORT="$SERVER_PORT",_SUB_NAME_PREFIX="$SUB_NAME_PREFIX",_UNIQUE_PREFIX="$UNIQUE_PREFIX",_ARTIFACT_REPO="$ARTIFACT_REPO",_REGION="$REGION",_API_KEY="$API_KEY"
+    _PROJECT_ID="$PROJECT_ID",_PORT="$SERVER_PORT",_SUB_NAME_PREFIX="$SUB_NAME_PREFIX",_UNIQUE_PREFIX="$UNIQUE_PREFIX",_ARTIFACT_REPO="$ARTIFACT_REPO",_REGION="$REGION",_REPO_REGION="$REPO_REGION",_API_KEY="$API_KEY",_INSTANCE_NAME_NUM="$INSTANCE_NAME_NUM"
