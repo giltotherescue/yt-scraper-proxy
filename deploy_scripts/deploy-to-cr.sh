9781d1ef39b476c6f61e49e5258207433aec29b9
maxafanasiev <80445276+maxafanasiev@users.noreply.github.com>
2025-02-26 15:59:00 +0200

    update scripts

--- deploy_scripts/deploy-to-cr.sh
+++ deploy_scripts/deploy-to-cr.sh
@@ -5,11 +5,19 @@ set -o allexport
 source ./.env set
 set +o allexport
 
-# Generate UNIQUE_PREFIX on the fly
-UNIQUE_PREFIX=$(date "+%Y-%m-%d-%H%M%S")
 
-# Run the gcloud builds submit command with substitutions
-gcloud builds submit --config deploy_scripts/deploy_cloudbuild_cr.yaml \
-  --project "$PROJECT_ID" \
-  --substitutions \
-    _PROJECT_ID="$PROJECT_ID",_PORT="$SERVER_PORT",_SUB_NAME_PREFIX="$SUB_NAME_PREFIX",_UNIQUE_PREFIX="$UNIQUE_PREFIX",_ARTIFACT_REPO="$ARTIFACT_REPO",_REGION="$REGION",_REPO_REGION="$REPO_REGION",_API_KEY="$API_KEY",_INSTANCE_NAME_NUM="$INSTANCE_NAME_NUM"
+IFS=',' read -r -a REGION_ARRAY <<< "$REGIONS"
+
+for REGION in "${REGION_ARRAY[@]}"; do
+    echo "Deploy region: $region"
+
+    ID=$(uuidgen | tr -d '-' | head -c 8 | tr '[:upper:]' '[:lower:]')
+    echo "Generated ID: $ID"
+
+    gcloud builds submit --config deploy_scripts/deploy_cloudbuild_cr.yaml \
+      --project "$PROJECT_ID" \
+      --substitutions \
+        _PROJECT_ID="$PROJECT_ID",_PORT="$SERVER_PORT",_SUB_NAME_PREFIX="$SUB_NAME_PREFIX",_ARTIFACT_REPO="$ARTIFACT_REPO",_REGION="$REGION",_REPO_REGION="$REPO_REGION",_API_KEY="$API_KEY",_INSTANCE_NAME_NUM="$ID" &
+done
+
+wait
